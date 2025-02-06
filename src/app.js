4d205a1ead3a99ae91bc46b5b5645351612b6e43
ak2patel <ankit.2201116cs@iiitbh.ac.in>
2025-02-06 18:34:10 +0530

    adding data to database

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 require("./config/database")
 const app = express();
 const connectDB = require('./config/database');
-const User = require('./models,user');
+const User = require('./models/user');
 
 
 
