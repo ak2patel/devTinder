774692896bc8f99454f3a06b207bc310f523e473
ak2patel <ankit.2201116cs@iiitbh.ac.in>
2025-04-20 12:30:24 +0530

    adding password change

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -2,6 +2,9 @@ const express = require("express");
 const profileRouter = express.Router();
 const {userAuth} = require("../middleware/auth");
 const { validateEditProfileData} = require("../utils/validation");
+const bcrypt = require("bcrypt");
+const User = require("../models/user");
+const validator = require("validator");
 require("dotenv").config();
 
 
@@ -29,10 +32,42 @@ profileRouter.patch("/profile/edit",userAuth,async(req,res)=>{
 
        res.send(`${user.firstName}, your profile updated successfully`);
        await  user.save();
-
+             
 
         
     } catch (err) {
+        res.status(400).send("ERROR: "+err.message);
+    }
+});
+
+profileRouter.patch("/profile/password",userAuth,async(req,res)=>{
+   try{
+       const {password} = req.body;
+       console.log("this is password " + password);
+       if(!validator.isStrongPassword(password)){
+        throw new Error("Enter a strong password");
+    }
+       
+       const passwordHash =  await bcrypt.hash(password,10);
+       console.log(passwordHash);
+      // const user = new User({password:passwordHash});
+       console.log(req.user);
+       //await user.save();
+
+       const user = await User.findByIdAndUpdate(
+        req.user._id,
+        { password: passwordHash },
+        { new: true }
+    );
+
+    if (!user) {
+        throw new Error("User not found");
+    }
+
+
+        res.send("Password added successfully");
+
+   }catch (err) {
         res.status(400).send("ERROR: "+err.message);
     }
 });
