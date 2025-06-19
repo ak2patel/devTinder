c04153b807b91cbac3168ec73f24428dcf1e571f
ak2patel <ankit.2201116cs@iiitbh.ac.in>
2025-06-20 01:40:27 +0530

    room id hashed

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -1,4 +1,12 @@
 const socket = require("socket.io");
+const crypto = require("crypto");
+
+const getSecretRoomId = (userId, targetUserId)=>{
+   return crypto
+    .createHash("sha256")
+    .update([userId,targetUserId].sort().join("_"))
+    .digest("hex");
+}
 
 const initializeSocket = (server)=>{
      const io = socket(server,{
@@ -10,15 +18,18 @@ const initializeSocket = (server)=>{
 io.on("connection",(socket)=>{
     //Handle Events
     socket.on("joinChat",({userId, targetUserId})=>{
-        const roomId = [userId,targetUserId].sort().join("_");
+       // const roomId = [userId,targetUserId].sort().join("_");
+        const roomId = getSecretRoomId(userId, targetUserId);
+       
         socket.join(roomId);
         console.log("room ID : ",roomId)
 
     });
 
     socket.on("sendMessage",({firstName, userId, targetUserId,text })=>{
-        const roomId = [userId,targetUserId].sort().join("_");
-        console.log(firstName+" "+text);
+        //const roomId = [userId,targetUserId].sort().join("_");
+        const roomId = getSecretRoomId(userId, targetUserId);
+    
         io.to(roomId).emit("messageReceived",{firstName,text});
         
 
